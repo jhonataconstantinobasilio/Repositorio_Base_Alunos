a44df2b42a093e5bda2fb705b0638f7ca7ca5156
jhonataconstantinobasilio <jhonataconstantinobasilio@gmail.com>
2025-10-22 11:24:30 -0300

    Add files via upload

---
+++ Modulo 2/APIs/app.py
@@ -0,0 +1,18 @@
+from flask import Flask , render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/test')
+def test():
+   return render_template('produtos.html')
+
+@app.route('/games')
+def games():
+   return render_template('styles.css')
+
+if __name__ == '__main__':
+ app.run(debug=True, host='127.0.0.1', port=5000)
